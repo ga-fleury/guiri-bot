5bb1d44fdf638dade8cfb10b0a1d55a2baca354c
Gabriel Fleury <gabrielfleury95@gmail.com>
2020-11-10 03:57:46 -0300

    remove token

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const botToken = require('./token');
+const botToken = require('./botoken');
 let bot = botToken.bot;
 
 client.login(bot);
--- token.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  bot: 'Nzc1NjAxMjU3NzEyMDU4NDE4.X6os9Q.A-OvUrUV8gDfHLo669MjbX6Vehc'
-};
