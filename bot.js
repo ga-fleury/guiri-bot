c4d0b36739ca9aa10df4e0b03123033b712e4853
Gabriel Fleury <gabrielfleury95@gmail.com>
2020-11-10 04:59:45 -0300

    banho final test

--- bot.js
+++ bot.js
@@ -12,9 +12,9 @@ client.on('ready', () => {
 
 client.on('message', message => {
   if (message.mentions.has(client.user.id)) {
-    if (message.content === 'banho') {
-      message.channel.send('https://cdn.discordapp.com/attachments/704138130130862081/751300367421276231/image0.jpg');
-    }
     message.channel.send('~ n ã ã o  m a n o ~  ಠ_ಠ');
+  }
+  if (message.content === 'banho') {
+    message.channel.send('https://cdn.discordapp.com/attachments/704138130130862081/751300367421276231/image0.jpg');
   }
 });
