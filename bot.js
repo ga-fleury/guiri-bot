b01d1c8c8f74609416d27189a39c6567af56fd2b
Gabriel Fleury <gabrielfleury95@gmail.com>
2020-11-10 05:12:28 -0300

    add guirikid

--- bot.js
+++ bot.js
@@ -18,6 +18,6 @@ client.on('message', message => {
     message.channel.send('https://cdn.discordapp.com/attachments/704138130130862081/751300367421276231/image0.jpg');
   }
   if (message.content === 'guirikid') {
-    message.channel.send('https://cdn.discordapp.com/attachments/745550754109980713/775632995498590259/unknown.png');
+    message.channel.send('https://cdn.discordapp.com/attachments/745550754109980713/775632995498590259/unknown.pnggi');
   }
 });
