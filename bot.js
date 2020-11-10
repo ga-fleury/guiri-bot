8d5a9f718d2d7b355ea014ce21caa22e829fb6d9
Gabriel Fleury <gabrielfleury95@gmail.com>
2020-11-10 04:24:08 -0300

    emoji correction

--- bot.js
+++ bot.js
@@ -12,6 +12,6 @@ client.on('ready', () => {
 
 client.on('message', message => {
   if (message.mentions.has(client.user.id)) {
-    message.channel.send(' ```~ n ã ã o  m a n o ~  ಠ_ಠ  ``` ');
+    message.channel.send(' ```~ n ã ã o  m a n o ~  ಠ_ಠ  😩  ``` ');
   }
 });
