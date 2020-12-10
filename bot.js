4fd642052739a4727b1b315ba62ea9ff55040b89
CagamerW <67083872+CagamerW@users.noreply.github.com>
2020-12-10 17:41:30 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('message', message => {
   }
   
   if (message.content === 'guirihelp' || message.content === 'Guirihelp') {
-    message.channel.send('Esse é o Guiribot, o bot do Guiri. Você pode brincar comigo usando as palavras: Guiritaro19, Guirichess, Guiribanho, Guirikid, Guirisexy, Guirigirl e Guiriaçai.\nE pra saber o meu endereço é só falar o meu nome e a palavra casa.');
+    message.channel.send('Esse é o Guiribot, o bot do Guiri.\nVocê pode brincar comigo usando as palavras: \n.Guiritaro19 \n.Guirichess \n.Guiribanho \n.Guirikid \n.Guirisexy \n.Guirigirl \n.Guiriaçai.\nE pra saber o meu endereço é só falar o meu nome e a palavra casa.');
   }
   
   if (message.mentions.has(client.user.id) && message.content.includes('banho')) {
