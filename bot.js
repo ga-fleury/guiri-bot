8b186043a6d7dada6d9453700193e4f07589084b
CagamerW <67083872+CagamerW@users.noreply.github.com>
2020-12-10 16:30:50 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,9 @@ client.on('message', message => {
   if (message.content === 'guiribanho' || message.content === 'Guiribanho') {
     message.channel.send('https://cdn.discordapp.com/attachments/704138130130862081/751300367421276231/image0.jpg');
   }
+  if (message.content === 'guiripai' || message.content === 'Guiripai') {
+    message.channel.send(https://cdn.discordapp.com/attachments/704138130130862081/786675304668200980/image0.jpg);
+  }
   if (message.content === 'guiritaro19' || message.content === 'Guiritaro19') {
     message.channel.send('Yea, just a lil dude tryin to make this world funnier with some crazy shit :p Btw i think that i prefer be called as "LMY" :3');
   }
